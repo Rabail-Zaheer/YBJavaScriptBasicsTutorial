2ddef3378548be263ff668073fc037f49ee776f0
Rabail Zaheer <73345859+Rabail-Zaheer@users.noreply.github.com>
2023-06-11 22:29:24 +0500

    All the events added

--- Form Events/index.js
+++ Form Events/index.js
@@ -13,16 +13,43 @@ select
 submit
 invalid*/
 
-/* focus */
+/* focus() */
 
 function focusfunction(element) {
   element.style.background = "orchid";
 }
 
-/* blur */
+/* blur() */
 
 function blurfunction(element) {
   element.style.background = "";
 }
 
-/* input */
+/* input() */
+
+function inputFunction(element) {
+  let x = element.value;
+  document.getElementById("test").innerHTML = x;
+}
+
+/* change() */
+
+function changeFunction(element) {
+  let x = element.value;
+  document.getElementById("test").innerHTML = x;
+}
+
+/* select() */
+
+function selectFunction() {
+  console.log("You selected a text");
+}
+
+/* submit() */
+
+function submitFunction() {
+  let x = document.getElementById("fname").value;
+  alert(`Hello ${x}, you have submitted the form!`);
+}
+
+/* invalid() */
