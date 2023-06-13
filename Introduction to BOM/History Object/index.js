bf1814436bd88a86f80bcabc9087cb35c72551ce
Rabail Zaheer <73345859+Rabail-Zaheer@users.noreply.github.com>
2023-06-13 19:00:32 +0500

    History Object and its methods added

---
+++ Introduction to BOM/History Object/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* HISTORY OBJECT
+
+Methods used with history Object are:
+
+forward()
+back()
+go()*/
+
+function backFunction() {
+  history.back();
+}
+
+function forwardFunction() {
+  history.forward();
+}
+
+function goFunction() {
+  history.go(1);
+}
